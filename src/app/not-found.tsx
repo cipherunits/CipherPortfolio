f6de6a31d5d70b2ab215ef6b12dfe3702d149289
Mohammad Taha Batoomi <tbatoomi@gmail.com>
2026-06-14 16:39:05 +0330

    feat(not-fount) : Creating a Nut Fund page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,7 +5,7 @@ function notfound() {
   return (
     <div className="flex flex-col justify-center items-center w-full h-[80vh] pb-10">
       <h1 className="font-bold text-[200px] text-white">404</h1>
-      <p className="text-(--color-stroke)">
+      <p className="text-(--color-stroke) text-center">
         The page you are looking for doesn’t exist or has been moved.
       </p>
       <Link href="/" className="mt-6">
