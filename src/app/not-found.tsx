918341ae2aebbe4bd40bc44b3387d9cd0f28fe5c
Mohammad Taha Batoomi <tbatoomi@gmail.com>
2026-06-14 16:29:56 +0330

    feat(not-fount) : Creating a Nut Fund page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,18 @@
+import Button from "@/components/shared/Button";
+import Link from "next/link";
+
+function notfound() {
+  return (
+    <div className="flex flex-col justify-center items-center w-full h-[80vh] pb-10">
+      <h1 className="font-bold text-[200px] text-white">404</h1>
+      <p className="text-(--color-stroke)">
+        The page you are looking for doesn’t exist or has been moved.
+      </p>
+      <Link href="/" className="mt-6">
+        <Button>Back to Landing page</Button>
+      </Link>
+    </div>
+  );
+}
+
+export default notfound;
