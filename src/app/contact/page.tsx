a36770c8542d6487f6360432cfaf51a3aa717996
Taha <MohammadTahaBatoomi>
2026-06-14 11:55:38 +0330

    Making the link in the header active based on the address

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,11 +2,10 @@ import ContactBox from "@/components/contacts/ContactBox";
 import ContactMedia from "@/components/contacts/ContactMedia";
 import ContactText from "@/components/contacts/ContactText";
 import NamePage from "@/components/shared/NamePage";
-import React from "react";
 
 function page() {
   return (
-    <div className="max-w-6xl mx-auto p-6 mt-12">
+    <div className="max-w-6xl mx-auto p-6 my-22">
       <div className="space-y-3">
         <NamePage />
         <p className="text-white [word-spacing:6px]">Who am i?</p>
--- src/components/shared/header/NavItem.tsx
+++ src/components/shared/header/NavItem.tsx
@@ -1,16 +1,25 @@
 import Link from "next/link";
 import { navItems } from "./Item";
+import { usePathname } from "next/navigation";
 
 export default function NavItem() {
-  return navItems.map((item: { name: string, link:string }) => (
-    <li
-      key={item.name}
-      className="text-gray-400 hover:text-white transition-colors cursor-pointer"
-    >
-      <Link className="flex gap-1" href={item.link}>
-        <span className="text-(--color-primery)">#</span>
-        <p>{item.name}</p>
-      </Link>
-    </li>
-  ));
-}
+  const pathname = usePathname();
+
+  const name = pathname === "/" ? "home" : pathname.split("/").filter(Boolean).pop();
+
+  return navItems.map((item: { name: string; link: string }) => {
+    const isActive = name === item.name;
+
+    return (
+      <li
+        key={item.name}
+        className="text-gray-400 hover:text-white transition-colors cursor-pointer"
+      >
+        <Link className="flex gap-1" href={item.link}>
+          <span className="text-(--color-primery)">#</span>
+          <p className={`${isActive ? "text-white" : ""}`}>{item.name}</p>
+        </Link>
+      </li>
+    );
+  });
+} 
