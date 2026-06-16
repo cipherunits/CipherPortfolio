96e4be2db362b8baa68b38e2f04b83e14e7fa702
Mohammad Taha Batoomi <tbatoomi@gmail.com>
2026-06-16 13:39:57 +0330

    fix url address for docs page

--- src/components/shared/header/Item.ts
+++ src/components/shared/header/Item.ts
@@ -1,5 +1,5 @@
 export const navItems = [
   { name: "home", link:"/"},
   { name: "contact", link:"/contact" },
-  { name: "docs", link:"https://cipherunits.github.io/CipherToken/getting-started/" },
+  { name: "docs", link:"https://cipherunits.github.io/CipherPortfolio/" },
 ];
